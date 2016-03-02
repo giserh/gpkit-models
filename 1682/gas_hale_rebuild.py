e138f67bdb242c2c1b6fef82d0e756f24d729a6f
1ozturkbe <1ozturkbe@gmail.com>
2016-03-02 10:01:30 -0500

    Added basic framework for VectorVariables (rebuild)

--- 1682/gas_hale_rebuild.py
+++ 1682/gas_hale_rebuild.py
@@ -1,16 +1,30 @@
 from numpy import pi
-from gpkit import Variable, Model, units
+from gpkit import VectorVariable, Variable, Model, units
 from gpkit.tools import te_exp_minus1
 import gpkit
 import numpy as np
+gpkit.settings['latex_modelname'] = False
 
-CL = Variable("CL")
-CD = Variable("CD")
-V = Variable("V", "m/s", "cruise speed")
-W = Variable("W", 200, "lbf", "weight")
-rho = Variable(r"\rho", 1.2, "kg/m^3", "air density")
-S = Variable("S", 190, "ft^2", "wing area")
-m = Model(V*(W/(CL/CD)),
-          [0.5*rho*CL*S*V**2 == W,
-           CL**1.5/CD <= 20])
-sol = m.solve()
+class GasPoweredHALE(Model):
+    def setup(self):
+        constraints = []
+        CD = Variable('C_D',1, '-', 'Drag coefficient')
+    	CL = Variable('C_L',1, '-', 'Lift coefficient')
+
+        W = VectorVariable(2,'W','lbf', 'Aircraft weight')
+        V = VectorVariable(2,'V',[20, 15], 'm/s','cruise speed')
+        rho = Variable(r"\rho", 1.2, "kg/m^3", "air density")
+        S = Variable("S", 190, "ft^2", "wing area")
+
+        constraints.extend([#0.5*rho*CL*S*np.power(V,2) == W,
+        	0.5*rho*CL*S*V[1]**2 == W[1],
+        	0.5*rho*CL*S*V[0]**2 == W[0],
+        	W >= 5*units('lbf'),
+            CL**1.5/CD <= 20])
+
+        objective = W
+        return objective, constraints
+
+if __name__ == "__main__":
+	M = GasPoweredHALE()
+	M.solve()
